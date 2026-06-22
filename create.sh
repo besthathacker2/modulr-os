0ea8ef8826e123a14a2b2e62213cf60eb411d664
besthathacker2 <263749894+besthathacker2@users.noreply.github.com>
2026-06-22 15:21:59 -0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,57 @@
-# 1. Standard compilation pass of all kernel system elements
+sudo apt update
+sudo apt install \
+    build-essential \
+    bison \
+    flex \
+    libgmp3-dev \
+    libmpc-dev \
+    libmpfr-dev \
+    texinfo \
+    wget \
+    nasm
+
+mkdir -p $HOME/cross
+export PREFIX="$HOME/cross"
+export TARGET=x86_64-elf
+export PATH="$PREFIX/bin:$PATH"
+
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.45.tar.xz
+tar -xf binutils-2.45.tar.xz
+
+mkdir build-binutils
+cd build-binutils
+
+../binutils-2.45/configure \
+    --target=$TARGET \
+    --prefix=$PREFIX \
+    --with-sysroot \
+    --disable-nls \
+    --disable-werror
+
+make -j$(nproc)
+make install
+
+cd ..
+
+wget https://ftp.gnu.org/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz
+tar -xf gcc-15.1.0.tar.xz
+
+mkdir build-gcc
+cd build-gcc
+
+../gcc-15.1.0/configure \
+    --target=$TARGET \
+    --prefix=$PREFIX \
+    --disable-nls \
+    --enable-languages=c,c++ \
+    --without-headers
+
+make all-gcc -j$(nproc)
+make all-target-libgcc -j$(nproc)
+
+make install-gcc
+make install-target-libgcc
+
 nasm -f elf64 loader.s -o loader.o
 x86_64-elf-g++ -c src/kernel.cpp -o kernel.o -ffreestanding -O3 -fno-exceptions -fno-rtti -std=c++17
 x86_64-elf-g++ -c src/paging.cpp -o paging.o -ffreestanding -O3 -fno-exceptions -fno-rtti
