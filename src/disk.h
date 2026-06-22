7587e9e22b72ddf0d3325c53176cc4e0e8fa270c
besthathacker2 <263749894+besthathacker2@users.noreply.github.com>
2026-06-22 14:25:20 -0700

    Update disk.h

--- src/disk.h
+++ src/disk.h
@@ -1,4 +1,5 @@
 // disk.h
+#pragma once
 #include <stdint.h>
 
 class DiskDriver {
@@ -18,38 +19,36 @@ private:
     }
 
 public:
-    // Reads a 512-byte sector from the hard drive using ATA PIO mode
     static void read_sector(uint32_t lba, uint8_t* target_buffer) {
         outb(0x1F6, (0xE0 | ((lba >> 24) & 0x0F)));
-        outb(0x1F2, 1); // Sector count (1 sector)
+        outb(0x1F2, 1); 
         outb(0x1F3, (uint8_t)lba);
         outb(0x1F4, (uint8_t)(lba >> 8));
         outb(0x1F5, (uint8_t)(lba >> 16));
-        outb(0x1F7, 0x20); // Command 0x20 = Read Sectors with retry
+        outb(0x1F7, 0x20); 
 
-        // Poll till the status drive drops BSY (Busy) and sets DRQ (Data Request)
         while ((inb(0x1F7) & 0x88) != 0x08);
 
-        // Read 256 words (512 bytes) from the data port
         uint16_t* ptr = (uint16_t*)target_buffer;
         for (int i = 0; i < 256; i++) {
             ptr[i] = inw(0x1F0);
         }
     }
+
+    // Reads multiple sequential sectors into a continuous destination memory block
+    static void read_file_sectors(uint32_t start_lba, uint32_t sector_count, uint8_t* dest_buffer) {
+        for (uint32_t i = 0; i < sector_count; i++) {
+            read_sector(start_lba + i, dest_buffer + (i * 512));
+        }
+    }
 };
 
-// Structures for parsing a true FAT storage layer
 struct FATDirectoryEntry {
-    uint8_t  filename[11];
+    uint8_t  filename[11]; // 8 chars name, 3 chars extension
     uint8_t  attribute;
-    uint8_t  reserved;
-    uint8_t  creation_time_ms;
-    uint16_t creation_time;
-    uint16_t creation_date;
-    uint16_t last_access_date;
-    uint16_t first_cluster_high;
+    uint8_t  reserved[10];
     uint16_t last_mod_time;
     uint16_t last_mod_date;
-    uint16_t first_cluster_low;
+    uint16_t first_cluster_low; // Maps to LBA start sector
     uint32_t file_size;
 } __attribute__((packed));
